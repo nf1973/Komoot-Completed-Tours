e15fb4e915775e5a8f0efd3a45001c9f1dbf1652
nf1973 <56644716+nf1973@users.noreply.github.com>
2024-08-08 23:41:34 +0200

    Enhancements to handle missing keys

--- tour.py
+++ tour.py
@@ -4,20 +4,22 @@ import math
 
 class Tour:
     def __init__(self, tour: Dict[str, Any]):
-        dt = self._extract_datetime(tour['date'])
-        self.tour_id = tour['id']
+        self.tour_id = tour.get('id')
+
+        dt = self._extract_datetime(tour.get('date', '1970-01-01T00:00:00Z'))
         self.date = dt.strftime('%Y-%m-%d')
         self.time = dt.strftime('%H:%M:%S')
-        self.distance = self._format_distance(tour['distance'])
-        self.name = tour['name']
-        self.start_lat = tour['start_point']['lat']
-        self.start_lng = tour['start_point']['lng']
-        self.start_alt = tour['start_point']['alt']
-        self.duration_in_seconds = tour['duration']
-        self.elevation_up = tour['elevation_up']
-        self.elevation_down = tour['elevation_down']
-        self.sport = tour['sport']
-        self.time_moving = tour['time_in_motion']
+
+        self.distance = self._format_distance(tour.get('distance', 0))
+        self.name = tour.get('name','No name found for this tour')
+        self.start_lat = tour.get('start_point', {}).get('lat', 0)
+        self.start_lng = tour.get('start_point', {}).get('lng', 0)
+        self.start_alt = tour.get('start_point', {}).get('alt', 0)
+        self.duration_in_seconds = tour.get('duration', 0)
+        self.elevation_up = tour.get('elevation_up', 0)
+        self.elevation_down = tour.get('elevation_down', 0)
+        self.sport = tour.get('sport', '')
+        self.time_moving = tour.get('time_in_motion', 0)
         self.map_url = self._remove_query_parameters(
             tour.get('vector_map_image', {}).get('src') or tour.get('map_image', {}).get('src')
         )
